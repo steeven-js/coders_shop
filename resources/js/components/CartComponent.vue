f100c0a8aab23242715dbf37cf53a4da5f76b8e6
steeven-js <jacques.steeven@gmail.com>
2023-06-11 06:37:46 -0400

    firt commit

---
+++ resources/js/components/CartComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    Panier: 0
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+    onMounted(() => console.log('hello panier'))
+</script>
---
+++ resources/views/cart/index.blade.php
@@ -0,0 +1,7 @@
+@extends('welcome')
+
+@section('content')
+    <h1>Mon panier</h1>
+
+    <cart-component />
+@endsection
